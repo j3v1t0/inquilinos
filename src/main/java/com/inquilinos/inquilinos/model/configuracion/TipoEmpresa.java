6c0b35acd9af4f98ff71076693f388ef88d53bcd
root <root@server.taveras.do>
2018-04-11 11:41:36 -0400

    Subiendo clase TipoEmpresa

---
+++ src/main/java/com/inquilinos/inquilinos/model/configuracion/TipoEmpresa.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.inquilinos.inquilinos.model.configuracion;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author root
+ */
+public class TipoEmpresa implements Serializable{
+
+    String idtipoempresa;
+    String tipoempresa;
+    private Set empresa = new HashSet(0);
+
+    public TipoEmpresa() {
+    }
+
+    public TipoEmpresa(String idtipoempresa, String tipoempresa) {
+        this.idtipoempresa = idtipoempresa;
+        this.tipoempresa = tipoempresa;
+    }
+
+    public String getIdtipoempresa() {
+        return idtipoempresa;
+    }
+
+    public void setIdtipoempresa(String idtipoempresa) {
+        this.idtipoempresa = idtipoempresa;
+    }
+
+    public String getTipoempresa() {
+        return tipoempresa;
+    }
+
+    public void setTipoempresa(String tipoempresa) {
+        this.tipoempresa = tipoempresa;
+    }
+
+    public Set getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Set empresa) {
+        this.empresa = empresa;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoEmpresa{" + "tipoempresa=" + tipoempresa + '}';
+    }
+    
+    
+}
